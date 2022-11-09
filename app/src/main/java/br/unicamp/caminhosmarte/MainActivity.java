48ff5e4688322079501d0e7697c35c7ba3765d2f
JHONATAN WILLIAN DOS SANTOS SILVA <u21686@academico.cotuca.unicamp.br>
2022-11-09 07:39:52 -0300

    startando

---
+++ app/src/main/java/br/unicamp/caminhosmarte/MainActivity.java
@@ -0,0 +1,19 @@
+package br.unicamp.caminhosmarte;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.app.Activity;
+import android.app.ListActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
